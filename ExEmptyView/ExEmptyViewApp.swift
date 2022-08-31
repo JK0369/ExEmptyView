6ef54be42d261d7bfd1697cb978703b8b286445e
jake <palatable7@naver.com>
2022-08-31 21:13:21 +0900

    Initial Commit

---
+++ ExEmptyView/ExEmptyViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExEmptyViewApp.swift
+//  ExEmptyView
+//
+//  Created by 김종권 on 2022/08/31.
+//
+
+import SwiftUI
+
+@main
+struct ExEmptyViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
